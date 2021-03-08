3938bc86da9b050740b7241291b96bd2892ba14c
yhatibi <yhatibi@gmail.com>
2021-03-08 16:55:19 +0100

    Initial commit

---
+++ src/main/java/ex1/HashTableTest.java
@@ -0,0 +1,12 @@
+package ex1;
+
+import org.junit.Test;
+
+public class HashTableTest {
+
+    @Test
+    public void addAtHash() {
+        HashTable ht = new HashTable();
+        System.out.println("hola");
+    }
+}
