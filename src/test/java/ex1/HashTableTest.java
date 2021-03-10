3bf65be66f77ec47d71d7a5bdc9bd7f831f0f6c2
Yasin Hatibi <yhatibi@gmail.com>
2021-03-10 23:40:56 +0100

    maven

---
+++ src/test/java/ex1/HashTableTest.java
@@ -0,0 +1,34 @@
+package ex1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class HashTableTest {
+
+    @org.junit.jupiter.api.Test
+    void count() {
+        HashTable hashTable = new HashTable();
+        Assertions.assertEquals(16, hashTable.size());
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void put() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void drop() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
